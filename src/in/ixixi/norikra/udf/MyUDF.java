001265a60af06d3d709d3817991ed7e4c007acf3
yuri odagiri <yuri_odagiri@dwango.co.jp>
2014-05-28 19:54:45 +0900

    fixed encoding.

--- src/in/ixixi/norikra/udf/MyUDF.java
+++ src/in/ixixi/norikra/udf/MyUDF.java
@@ -12,25 +12,24 @@ import java.nio.file.StandardOpenOption;
 public final class MyUDF {
 
   public static String raw(final String source) {
+
     writeSourceToFile(source,"/tmp/norikra-udf-raw.log");
 
     return source;
   }
 
   public static String convertLatin1ToUTF8(final String source) {
-    String utf8Str = null;
-    try{
-      utf8Str = new String(source.getBytes("ISO-8859-1"), "UTF-8");
-      writeSourceToFile(utf8Str,"/tmp/norikra-udf-convertLatin1ToUTF8.log");
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    }
+
+    String utf8Str = new String(source.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+    writeSourceToFile(utf8Str,"/tmp/norikra-udf-convertLatin1ToUTF8.log");
 
     return utf8Str;
   }
 
   private static void writeSourceToFile(final String line, String dstPath){
+
     Path dst = Paths.get(dstPath);
+
     try (BufferedWriter bw = Files.newBufferedWriter(dst,StandardCharsets.UTF_8, StandardOpenOption.CREATE)) {
       bw.write(line);
 
